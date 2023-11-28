40642eace35c7cfdd22df957c27ec2bf11357336
mjblue031 <mjbluerosales@gmail.com>
2023-11-28 12:15:19 +0800

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
 //import lodash
 import throttle from 'lodash.throttle';
-
-const throttle = require('lodash.throttle');
 try {
+  const throttle = require('lodash.throttle');
+
   const iframe = document.querySelector('iframe');
   const player = new Vimeo.Player(iframe);
 
@@ -14,9 +14,15 @@ try {
   };
 
   //get the time update
-
   player.on('timeupdate', throttle(throttledTime, 1000));
-  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+  let currTime = localStorage.getItem('videoplayer-current-time');
+  if (currTime <= 0) {
+    player.setCurrentTime(0); 
+  }
+  else {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time')); 
+  }
 } catch (error) {
   console.log(error);
 }
